098c558507c2993958ca26c89b66022d9ca53755
mandeep6ill <mandeep6ill@gmail.com>
2016-11-12 19:14:08 +0530

    [Ignore] Random Test

--- tryouts/2011/bh08ti/1205/Main.java
+++ tryouts/2011/bh08ti/1205/Main.java
@@ -12,13 +12,13 @@ public class Main {
 	public void work (){
 		int N;
 		int R;
-		int tree[];
 
 		Scanner scan = new Scanner(System.in);
 		
 		N = scan.nextInt();
 		R = scan.nextInt();		
 
+        int tree[];
 		tree = new int[N];
 		for (int i = 0; i<N; i++) {
 			tree[i] = scan.nextInt();
